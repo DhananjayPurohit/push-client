0276903607e08866e08443856141dc2f87d1f9ee
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-07-07 23:28:36 +0530

    Updates

---
+++ precache-manifest.2a96cd7df0343ce76ee0203e23b48693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520efaac9118894af20a25d9b30c6830",
+    "url": "/push-client/index.html"
+  },
+  {
+    "revision": "eba2021767f6180a07a7",
+    "url": "/push-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b90d134c1099d37a2dd1",
+    "url": "/push-client/static/js/2.5d6dd0c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/push-client/static/js/2.5d6dd0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba2021767f6180a07a7",
+    "url": "/push-client/static/js/main.08d9bb21.chunk.js"
+  },
+  {
+    "revision": "5daa5c66645d9c2e9e93",
+    "url": "/push-client/static/js/runtime-main.341c3528.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/push-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
