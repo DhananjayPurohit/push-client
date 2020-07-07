ee848499c8fb46afc3b5c7c9d7b1ac86dd702bed
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-07-07 23:35:24 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,3 +37,17 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/p
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
+self.addEventListener('push', event => {
+    const data = event.data.json()
+    console.log('New notification', data)
+    const options = {
+      body: data.body,
+      actions: data.actions
+    }
+    console.log(options)
+    console.log(Notification.maxActions)
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  })
+  
